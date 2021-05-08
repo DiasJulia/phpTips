e5fa7ec6861e566c228b13a650807b47d3a9d811
MistoQuente082 <julia@orditi.com>
2021-05-08 19:27:46 -0300

    Initial Setup

---
+++ vendor/composer/installed.php
@@ -0,0 +1,46 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'vendor_name/phptips',
+    'dev' => true,
+  ),
+  'versions' => 
+  array (
+    'coffeecode/datalayer' => 
+    array (
+      'pretty_version' => '1.1.8',
+      'version' => '1.1.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '52e209190752eccc0bfd8ff3abcdd607ed4c01c1',
+      'dev-requirement' => false,
+    ),
+    'league/plates' => 
+    array (
+      'pretty_version' => 'v4.0.0-alpha',
+      'version' => '4.0.0.0-alpha',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f53f4c1fa4bf307d0f3858a348172ff4faf7669a',
+      'dev-requirement' => false,
+    ),
+    'vendor_name/phptips' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+      'dev-requirement' => false,
+    ),
+  ),
+);
